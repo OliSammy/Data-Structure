dc5c527443023d41070148e051c637e541d9aa64
TheMage <themage@localhost.localdomain>
2023-04-18 16:37:46 -0300

    Upando a quaestão 9

---
+++ Questão9/pilha.h
@@ -0,0 +1,19 @@
+#ifndef PILHA_H
+#define PILHA_H
+#define MAX 100
+#include <stdbool.h>
+typedef struct
+{
+    int topo;
+    char itens[MAX];
+}Pilha;
+
+Pilha* inicializar();
+bool pilhavazia(Pilha *p);
+bool pilhacheia(Pilha *p);
+void push(Pilha *p,char itens);
+char pop(Pilha *p);
+bool verificarpareteses(char *expr);
+
+
+#endif
